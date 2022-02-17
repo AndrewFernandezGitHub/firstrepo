1da1e534df52cc8e7f7478b159dff84e044337f7
AndrewFernandeGitHub <drew12f@gmail.com>
2022-02-16 21:51:00 -0500

    New Commit CHange

--- src/gitpractice/gitbatch12.java
+++ src/gitpractice/gitbatch12.java
@@ -5,6 +5,7 @@ public class gitbatch12 {
 	public static void main(String[] args) {
 		System.out.println("the wire");
 		System.out.println("hre;;;;eee");
+		System.out.println("another change to code to transfer ");
 		// TODO Auto-generated method stub
 
 	}
