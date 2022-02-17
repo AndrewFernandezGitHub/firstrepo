e1204f8ebb60f5ff3f5264ab81ca329021540901
AndrewFernandeGitHub <drew12f@gmail.com>
2022-02-16 21:39:51 -0500

    I have updated code

--- src/gitpractice/gitbatch12.java
+++ src/gitpractice/gitbatch12.java
@@ -3,6 +3,8 @@ package gitpractice;
 public class gitbatch12 {
 
 	public static void main(String[] args) {
+		System.out.println("the wire");
+		System.out.println("hre;;;;eee");
 		// TODO Auto-generated method stub
 
 	}
